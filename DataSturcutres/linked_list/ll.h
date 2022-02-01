4ef053650c49c3c695b4a026ec8ef549df38aff4
Vaibhav khare <vaibhav.kharee@gmail.com>
2022-02-01 11:35:40 +0530

    1. introducing length of linked list 2. Loop creation and detection

--- DataSturcutres/linked_list/ll.h
+++ DataSturcutres/linked_list/ll.h
@@ -22,6 +22,9 @@ accessing elements
 #define USING_STACK 2
 #define USING_ARRAY 3
 
+#define DEBUG 0
+#define MIN_LEN_FOR_LOOP 4
+
 typedef struct s_node {
     int ele;
     struct s_node *next;
@@ -33,17 +36,22 @@ typedef struct d_node {
     struct d_node *prev;
 } d_node_t;
 
-extern void insert_s(s_node_t **head, int ele); //add at the end
-extern void delete_s(s_node_t **head, int ele); // delete the selected node
+extern void insert_s(s_node_t **head, int ele, int *len); //add at the end
+extern void delete_s(s_node_t **head, int ele, int *len); // delete the selected node
 extern void print_s(s_node_t *head); // prints the ll
 extern void reverse_s (s_node_t **head, int type);
-extern s_node_t* create_list(s_node_t *head);
-extern s_node_t* delete_list(s_node_t *head);
+extern int create_list(s_node_t **head);
+extern int delete_list(s_node_t **head, int len);
 
 extern void insert_d(s_node_t **head, int ele);
 extern void delete_d(s_node_t *head, int ele);
 extern void print_d(s_node_t *head);
 
 extern s_node_t *alloc_new_node(int ele); // allocats a new SLL node
+
+//loop related functions
+void ll_loop(s_node_t *head, int len);
+void create_loop(s_node_t *head, int len);
+void detect_loop(s_node_t *head, int len);
 
 #endif //__ll_h__
