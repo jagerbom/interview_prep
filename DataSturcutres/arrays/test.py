d60c740315b8f2514e31fdd93b9ed38c9de9e4f0
Vaibhav khare <vaibhav.kharee@gmail.com>
2022-04-25 23:44:17 +0530

    system design

--- DataSturcutres/arrays/test.py
+++ DataSturcutres/arrays/test.py
@@ -2,14 +2,10 @@ from hashlib import new
 from operator import ne
 from pickle import TRUE
 from traceback import print_tb
-
+from queue import Queue
 
 if __name__ == '__main__':
-        arr = [2,3,4,5]
-        dict_new = {}
-        for x in range(len(arr)):
-                if ((arr[x]*2 in dict_new) or (arr[x]%2 == 0 and arr[x]/2 in dict_new)):
-                        print("True")
-                else:
-                        dict_new[arr[x]] = x
-        print(dict_new)                      
+        q = Queue(maxsize=3)
+        if (q.empty() is True):
+                print("inside if")
+                q.get()                   
